efd48ac239d3ab68e89ec900c88261482c666f3b
CodeEngineer NL <cp@codeengineer.nl>
2020-12-08 20:39:58 +0100

    Day 8, part 2

--- src/nl/codeengineer/projects/adventofcode2020/Day8.java
+++ src/nl/codeengineer/projects/adventofcode2020/Day8.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day8 {
 
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("./input/day8.txt"));
         part1(lines);
-
+        part2(lines);
     }
 
     private static class Instruction {
@@ -36,6 +37,76 @@ public class Day8 {
         }
 
         System.out.println(acc);
+    }
+
+    private static class HistoryItem {
+        Instruction instruction;
+        int pc = 0;
+        int acc = 0;
+    }
+
+    private static void part2(List<String> lines) {
+        int acc = 0;
+        int pc = 0;
+
+        List<Instruction> instructions = lines.stream().map(Day8::parseLine).collect(Collectors.toList());
+
+        Set<Integer> executedLines = new HashSet<>();
+        Stack<HistoryItem> history = new Stack<>();
+
+        int changedLine = -1;
+
+        while (pc < instructions.size()) {
+            Instruction instruction = instructions.get(pc);
+            executedLines.add(pc);
+
+            HistoryItem historyItem = new HistoryItem();
+            historyItem.acc = acc;
+            historyItem.pc = pc;
+            historyItem.instruction = instruction;
+            history.push(historyItem);
+
+
+            switch (instruction.name) {
+                case "acc": acc += instruction.value; pc++; break;
+                case "jmp": pc += instruction.value; break;
+                default: pc++;
+            }
+
+            if (executedLines.contains(pc)) {
+                while (changedLine > -1) {
+                    // Already changed. Find changed item in history and pop it off
+                    HistoryItem lastItem = history.pop();
+                    if (changedLine == lastItem.pc) {
+                        changedLine = -1;
+                        changeInstruction(instructions, lastItem.pc);
+                    }
+                }
+
+                HistoryItem lastItem = history.pop();
+                while (lastItem.instruction.name.equals("acc")) {
+                    lastItem = history.pop();
+                }
+
+                changeInstruction(instructions, lastItem.pc);
+                changedLine = lastItem.pc;
+                pc = lastItem.pc;
+                acc = lastItem.acc;
+            }
+        }
+
+        System.out.println(acc);
+
+    }
+
+    private static void changeInstruction(List<Instruction> instructions, int index) {
+        Instruction instruction = instructions.remove(index);
+
+        Instruction newInstruction = new Instruction();
+        newInstruction.value = instruction .value;
+        newInstruction.name = instruction.name.equals("jmp") ? "nop" : "jmp";
+
+        instructions.add(index, newInstruction);
     }
 
     private static Instruction parseLine(String line) {
