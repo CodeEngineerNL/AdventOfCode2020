17135c5969814ec9ce313bef78dcc829940358e2
CodeEngineer NL <cp@codeengineer.nl>
2020-12-14 21:04:28 +0100

    Day 10, part 2

--- src/nl/codeengineer/projects/adventofcode2020/Day10.java
+++ src/nl/codeengineer/projects/adventofcode2020/Day10.java
@@ -3,8 +3,7 @@ package nl.codeengineer.projects.adventofcode2020;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day10 {
@@ -12,19 +11,21 @@ public class Day10 {
 
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("./input/day10.txt"));
-        //List<String> lines = getTestInput();
 
         List<Integer> ratings = lines.stream().map(Integer::parseInt).collect(Collectors.toList());
-        ratings.add(0, 0);
-
         ratings.sort(Integer::compareTo);
 
+        ratings.add(0, 0);
+        ratings.add(ratings.get(ratings.size() - 1) + 3);
+
+
         part1(ratings);
+        part2(ratings);
     }
 
     private static void part1(List<Integer> ratings) {
         int diff1 = 0;
-        int diff3 = 1;
+        int diff3 = 0;
 
         for (int i = 0; i < ratings.size() - 1; i++) {
             switch (ratings.get(i+1) - ratings.get(i)) {
@@ -34,6 +35,86 @@ public class Day10 {
         }
 
         System.out.println(diff1 * diff3);
+    }
+
+    private static class GraphItem {
+        public int num;
+        public List<GraphItem> next = new ArrayList<>();
+    }
+
+    private static void part2(List<Integer> ratings) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int i = 0; i < ratings.size(); i++) {
+            int num = ratings.get(i);
+            List<Integer> items = new ArrayList<>();
+            for (int j = i + 1; j < ratings.size() && (ratings.get(j) - num < 4) ; j++) {
+                items.add(ratings.get(j));
+            }
+            graph.put(num, items);
+        }
+
+        System.out.println(countPaths(ratings, graph));
+    }
+
+    private static long countPaths(List<Integer> ratings,  Map<Integer, List<Integer>> graph) {
+        Map<Integer, Long> calculated = new HashMap<>();
+
+        for (int i = ratings.size() - 1; i >= 0; i--) {
+            int num = ratings.get(i);
+            if (!calculated.containsKey(num)) {
+
+                if (graph.get(num).size() < 1) {
+                    calculated.put(num, 1l);
+                } else {
+                    long count = 0;
+                    for (Integer nextNum: graph.get(num)) {
+                        count = count + calculated.get(nextNum);
+                    }
+                    calculated.put(num, count);
+                }
+            }
+        }
+
+        return calculated.get(0);
+    }
+
+
+    private static List<String> getTestInput2() {
+        String input = "28\n" +
+                "33\n" +
+                "18\n" +
+                "42\n" +
+                "31\n" +
+                "14\n" +
+                "46\n" +
+                "20\n" +
+                "48\n" +
+                "47\n" +
+                "24\n" +
+                "23\n" +
+                "49\n" +
+                "45\n" +
+                "19\n" +
+                "38\n" +
+                "39\n" +
+                "11\n" +
+                "1\n" +
+                "32\n" +
+                "25\n" +
+                "35\n" +
+                "8\n" +
+                "17\n" +
+                "7\n" +
+                "9\n" +
+                "4\n" +
+                "2\n" +
+                "34\n" +
+                "10\n" +
+                "3\n";
+
+        return Arrays.asList(input.split("\\n"));
+
     }
 
     private static List<String> getTestInput() {
